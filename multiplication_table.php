a4a48a684f721fee4118bcd183976a97846ec355
Hikaru-Hrn <geniysenkou@gmail.com>
2026-03-11 11:29:55 +0000

    multiplication table update

--- multiplication_table.php
+++ multiplication_table.php
@@ -5,6 +5,8 @@
     <style>
         table { border-collapse: collapse}
 	td { border: 1px solid #ccc; padding: 5px; width: 30px; text-align: center}
+	th { border: 1px solid #ccc; padding: 5px; width: 30px; background-color: #34495e; color: white}
+	.diagonal {background-color: #ebf5fb}
     </style>
 </head>
 <body>
@@ -14,11 +16,23 @@ echo '<table>';
 for ($i = 1; $i <= 10; $i++){
 	echo '<tr>';
 	for ($j = 1; $j<= 10; $j++){
-		echo '<td>' . $i*$j . '</td>';
+		if ($i === 1 and $j===1){
+			echo '<th>' . ' ' . '</th>';
+		}
+		else if ($i === 1 or $j === 1){
+			echo '<th>' . $i*$j . '</th>';
+		}
+		else if ($i === $j)
+		{
+			echo '<td class="diagonal">' . $i*$j . '</td>';
+		}
+		else{
+			echo '<td>' . $i*$j . '</td>';
+		}
 	}
-	echo '</td>';
+	echo '</tr>';
 }
-echo '</table>'
+echo '</table>';
 ?>
 
 </body>
